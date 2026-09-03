47a527e61913f76aae441ff85babf28b059e12f5
G-Uday-kumar <uday.kumar@bugasura.io>
2026-09-03 21:31:11 +0530

    Correct echo statements and calculate sum
    Fix variable name and add sum calculation.

--- app/Hello.php
+++ app/Hello.php
@@ -1,5 +1,9 @@
 <?php
 echo "hello welcome to the this world";
-echo "hey i also tried to changes inside the file with a sub branch and now i need to merge this file"
-echo "hello"
+echo "hey i also tried to changes inside the file with a sub branch and now i need to merge this file";
+echo "hello";
+$num1=5;
+$num2=7;
+$summ=$num1+$num2;
+echo $sum;
 ?>
