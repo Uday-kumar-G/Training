3d9e3a3a664431afb990bc90bde84375ef5f25d7
G-Uday-kumar <uday.kumar@bugasura.io>
2026-09-24 15:13:40 +0530

    Hello.php file was added

--- app/Hello.php
+++ app/Hello.php
@@ -1,9 +1,4 @@
 <?php
-echo "hello welcome to the this world";
-echo "hey i also tried to changes inside the file with a sub branch and now i need to merge this file";
-echo "hello";
-$num1=5;
-$num2=7;
-$summ=$num1+$num2;
-echo $sum;
+echo "hello G Uday Kumar\n";
+
 ?>
